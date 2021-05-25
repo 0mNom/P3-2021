a2d1993049c53f86a86d9072b61507e4acad3a22
0mNom <62939121+0mNom@users.noreply.github.com>
2021-05-25 11:07:53 +0200

    eek

--- P3 2021/Assets/Scripts/Seeker.cs
+++ P3 2021/Assets/Scripts/Seeker.cs
@@ -5,14 +5,27 @@ using UnityEngine;
 public class Seeker : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject cam;
     void Start()
     {
-        
+        gameObject.SetActive(false);
+        cam.SetActive(false);
+        StartCoroutine("find");
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    IEnumerator find()
+    {
+        yield return new WaitForSeconds(30f);
+        gameObject.SetActive(true);
+        cam.SetActive(true);
+
     }
 }
